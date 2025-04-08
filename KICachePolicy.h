bbb77490c779a992c18511144288183e0848d6c3
Ubuntu <ubuntu@localhost.localdomain>
2025-04-08 19:57:43 +0800

    Policy_all

--- KICachePolicy.h
+++ KICachePolicy.h
@@ -1,2 +1,20 @@
-#include <iostream>
+#pragma once
 
+namespace KamaCache
+{
+
+template <typename Key, typename Value>
+class KICachePolicy
+{
+public:
+    virtual ~KICachePolicy() {};
+
+    virtual void put(Key key, Value value) = 0;
+
+    virtual bool get(Key key, Value& value) = 0;
+
+    virtual Value get(Key key) = 0;
+
+};
+
+}
